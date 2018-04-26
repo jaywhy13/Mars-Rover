6fb07d89e6b3595459d2675df0ef6f22042b5221
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-04-26 01:28:13 -0500

    Added in the boundary constraint class that makes sure that we're not going beyond our boundaries. Also added an is_move_valid that checks to see if we can move

--- rover/plateau.py
+++ rover/plateau.py
@@ -1,3 +1,20 @@
+from rover.constraint import Constraint
+
+
+class BoundaryConstraint(Constraint):
+    """ This ensures that no movement is made outside of the plateau
+    """
+    def __init__(self, plateau=None):
+        self.plateau = plateau
+
+    def can_move(self, x=None, y=None, new_x=None, new_y=None, heading=None):
+        size = self.plateau.size
+        return all([
+            new_x >= 0 and new_x <= size[0],
+            new_y >= 0 and new_y <= size[1],
+        ])
+
+
 class Plateau(object):
     """ An encapsulation of the rectangular region on Mars
     """
@@ -17,3 +34,15 @@ class Plateau(object):
         rover.constraint = self
         self.rovers.append(rover)
 
+    def is_move_valid(self, rover=None):
+        """ Checks our constraints to determine whether the given rover
+            can move
+        """
+        new_x, new_y = rover.coords_after_move
+        x, y = rover.coords
+        for constraint in self.constraints:
+            if not constraint.can_move(
+                    x=x, y=y,
+                    new_x=new_x, new_y=new_y, heading=rover.heading):
+                return False
+        return True
