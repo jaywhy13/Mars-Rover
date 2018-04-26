a14b74e85a94d78838e36006effbc7410608ebf8
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-04-26 01:31:44 -0500

    Added tests for rotation

--- tests.py
+++ tests.py
@@ -22,3 +22,30 @@ class TestRover(unittest.TestCase):
         self.assertEqual(mars_rover.x, 0)
         self.assertEqual(mars_rover.y, 0)
         self.assertEqual(mars_rover.heading, HEADING_NORTH)
+
+    def test_rotation(self):
+        mars_rover = Rover()
+        expected_headings = [
+            HEADING_WEST, HEADING_SOUTH, HEADING_EAST, HEADING_NORTH]
+        for i, expected_heading in enumerate(expected_headings):
+            mars_rover.turn_left()
+            self.assertEqual(mars_rover.heading, expected_headings[i])
+        expected_headings = [
+            HEADING_EAST, HEADING_SOUTH, HEADING_WEST, HEADING_NORTH]
+        for i, expected_heading in enumerate(expected_headings):
+            mars_rover.turn_right()
+            self.assertEqual(mars_rover.heading, expected_headings[i])
+        # we should be back at North
+        self.assertEqual(mars_rover.heading, HEADING_NORTH)
+        mars_rover.turn_left()
+        mars_rover.turn_right()
+        self.assertEqual(mars_rover.heading, HEADING_NORTH)
+        mars_rover.turn_right()
+        mars_rover.turn_left()
+        self.assertEqual(mars_rover.heading, HEADING_NORTH)
+        mars_rover.reset()
+        mars_rover.turn_around()
+        self.assertEqual(mars_rover.heading, HEADING_SOUTH)
+        mars_rover.turn_right()
+        mars_rover.turn_around()
+        self.assertEqual(mars_rover.heading, HEADING_EAST)
