ddf1b5c95df2a1101c5a962d450151545fa164bf
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-04-26 01:31:35 -0500

    Tested default kwargs for the Rover

--- tests.py
+++ tests.py
@@ -13,3 +13,12 @@ from rover.constants import (
 )
 from rover.plateau import Plateau, BoundaryConstraint
 from rover.parser import Parser
+
+
+class TestRover(unittest.TestCase):
+
+    def test_default_kwargs(self):
+        mars_rover = Rover()
+        self.assertEqual(mars_rover.x, 0)
+        self.assertEqual(mars_rover.y, 0)
+        self.assertEqual(mars_rover.heading, HEADING_NORTH)
