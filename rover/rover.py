60e8fd081b01334bf3992204dd8dd29f14ba1c17
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-04-26 01:29:33 -0500

    Added the init method. Takes a plateau, coords and heading

--- rover/rover.py
+++ rover/rover.py
@@ -9,3 +9,12 @@ from collections import namedtuple
 Coords = namedtuple('Coords', ['x', 'y'])
 
 
+class Rover(object):
+    """ An encapsulation of the mars vehicle
+    """
+
+    def __init__(self, x=0, y=0, heading=HEADING_NORTH, plateau=None):
+        self.x = x
+        self.y = y
+        self.heading = heading
+        self.plateau = plateau
